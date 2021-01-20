03871cf60ca7ff73fde242f324adaeaa6416e0a5
John Mejia <jhondario.09@gmail.com>
2021-01-20 17:14:19 -0500

    Added contact for desktop

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -8,20 +8,22 @@ import styles from './Contact.module.scss';
 
 const Contact = () => {
     return (
-        <div className={styles.content}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>Get in Touch</h1>
-            </header>
-            <section className={styles.main}>
-                <h3 className={styles.subtitle}>I'd love to hear from you!</h3>
-                <p className={styles.text}>
-                   I believe in long-term partnerships to create scalable and lasting experiences.
-                </p>
-                <div className={styles.touch}>
-                    <SocialLinks />
-                    <span className={styles['touch__email']}>jmejiah.09@gmail.com</span>
-                </div>
-            </section>
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <header className={styles.header}>
+                    <h1 className={styles.title}>Get in touch.</h1>
+                </header>
+                <section className={styles.main}>
+                    <h3 className={styles.subtitle}>I'd love to hear from you!</h3>
+                    <p className={styles.text}>
+                    I believe in long-term partnerships to create scalable and lasting experiences.
+                    </p>
+                    <div className={styles.touch}>
+                        <SocialLinks />
+                        <span className={styles['touch__email']}>jmejiah.09@gmail.com</span>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
