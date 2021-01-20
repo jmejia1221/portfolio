7320f7a893012c0dcc0a5703bf95ff2d8939fba9
John Mejia <jhondario.09@gmail.com>
2021-01-20 16:43:50 -0500

    Added about and projects responsive

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -62,24 +62,33 @@ const About = () => {
     };
 
     return (
-        <div className={styles.content}>
-            <h1 className={styles.title}>About Me</h1>
-            <article className={styles.article}>
-                <p className={styles.text}>Professionally connected with the web development industry and information
-                technology for over 8 years. I've been working at Anomali inc.
-                <a className={styles.link} target="_blank" href="https://www.anomali.com/">anomali.com</a> for last 5 years.</p>
-                <p className={styles.text}>Well-organised person, problem solver with high attention to detail.
-                Seeking challenges to create easy-to-navigate websites, create them from scratch or
-                redesign and build them in web development technologies.</p>
-                <p className={styles.text}><strong>I'm always up for new challenges, where I can collaborate with people and companies to
-                get their product ideas rolling.</strong></p>
-                <p className={styles.text}>Feel free to check <a className={styles.link} target="_blank" href="">my resume</a>.</p>
-            </article>
-            <div className={styles.skillsAction}>
-                <Button clicked={modalHandler}>
-                    Skills & Expertise
-                    <FontAwesomeIcon className={styles.arrowIcon} icon={faChevronRight} />
-                </Button>
+        <>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>About Me</h1>
+                    <article className={styles.article}>
+                        <p className={`${styles.text} ${styles.greets}`}><strong>- Hello</strong></p>
+                        <p className={styles.text}>I'm professionally connected with the web development industry and information
+                        technology for over 8 years. I've been working at Anomali inc.
+                        <a className={styles.link} target="_blank" href="https://www.anomali.com/">anomali.com</a> for last 5 years.</p>
+                        <p className={styles.text}>Well-organised person, problem solver with high attention to detail.
+                        Seeking challenges to create easy-to-navigate websites, create them from scratch or
+                        redesign and build them in web development technologies.</p>
+                        <p className={`${styles.text} ${styles.legend}`}>
+                            <strong>I'm always up for new challenges, where I can collaborate with people and companies to
+                        get their product ideas rolling.</strong>
+                        </p>
+                        <p className={`${styles.text} ${styles.legend}`}>
+                            Feel free to check <a className={styles.link} target="_blank" href="">my resume</a>.
+                        </p>
+                    </article>
+                    <div className={styles.skillsAction}>
+                        <Button clicked={modalHandler}>
+                            Skills & Expertise
+                            <FontAwesomeIcon className={styles.arrowIcon} icon={faChevronRight} />
+                        </Button>
+                    </div>
+                </div>
             </div>
             <Modal
                 closeModal={modalHandler}
@@ -115,7 +124,7 @@ const About = () => {
                     </section>
                 </div>
             </Modal>
-        </div>
+        </>
     );
 };
 
