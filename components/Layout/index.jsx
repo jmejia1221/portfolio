70f9666abbdf5c4cdda72f437b24b5afbd44dbb1
John Mejia <jhondario.09@gmail.com>
2021-02-09 03:58:01 -0500

    Adding favicon and resume

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Head from 'next/head';
 
 // Components
 import Header from '../Header';
 import Nav from '../Nav';
 import NavItem from '../Nav/NavItem';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 // CSS
 import styles from './Layout.module.scss';
@@ -11,6 +13,12 @@ import styles from './Layout.module.scss';
 const Layout = (props) => {
     return (
         <div>
+            <Head>
+                <title>John Mejia</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            </Head>
             <Header>
                 <Nav>
                     <NavItem href="/">Home</NavItem>
